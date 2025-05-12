41845a9714aeca8c026218f3321fa0c9bb31e6eb
Nicolas Modrzyk <hellonico@gmail.com>
2025-05-12 14:08:37 +0900

    adding breeze submodule

--- pyjama-philosophers/src/pyjama/games/handlers/echo.clj
+++
@@ -1,9 +0,0 @@
-(ns pyjama.games.handlers.echo)
-
-(defn handler [data]
-  (let [
-        messages (:messages data)
-        last-message (:content (last messages))
-        ]
-    {:model   "echo"
-     :message {:role "user" :content (last-message)}}))
--- pyjama-philosophers/src/utils/core.clj
+++ pyjama-philosophers/src/utils/core.clj
@@ -1,7 +1,7 @@
 (ns utils.core
   (:require [clj-http.client :as http])
   (:import
-    (java.net Socket)
+    (java.net NetworkInterface Socket)
     (clojure.lang Atom)))
 
 (defn deep-deref [x]
@@ -12,29 +12,30 @@
     :else x))
 
 
-(defn port-open? [host port]
-  (try
-    (with-open [_ (Socket. host port)]
-      true)
-    (catch Exception _ false)))
+;(defn port-open? [host port]
+;  (try
+;    (with-open [_ (Socket. host port)]
+;      true)
+;    (catch Exception _ false)))
 
-(defn closed-ports [state]
-  (->> (:people state)
-       (filter (fn [{:keys [url]}]
-                 (let [[_ host port] (re-matches #"http://([^:]+):(\d+)" url)
-                       port (Integer/parseInt port)]
-                   (not (port-open? host port)))))
-       (map :name)))
+;(defn closed-ports [state]
+;  (->> (:people state)
+;       (filter (fn [{:keys [url]}]
+;                 (let [[_ host port] (re-matches #"http://([^:]+):(\d+)" url)
+;                       port (Integer/parseInt port)]
+;                   (not (port-open? host port)))))
+;       (map :name)))
 
 
 (defn url-alive? [url]
   (try
-    (println url)
     (let [response (http/get url {:throw-exceptions false :timeout 2000})]
       ;(<= 200 (:status response) 399)
       true ; TODO: check response code later
       ) ;; Alive if status code is in 200-399 range
-    (catch Exception _ false)))
+    (catch Exception e
+      ;(.printStackTrace e)
+      false)))
 
 ;
 ;(defn mark-alive [app-state]
@@ -51,3 +52,13 @@
     (swap! person-atom
            (fn [{:keys [url] :as person}]
              (assoc person :alive (url-alive? url))))))
+
+(defn get-non-local-ip []
+  (->> (NetworkInterface/getNetworkInterfaces)
+       (enumeration-seq)
+       (mapcat #(enumeration-seq (.getInetAddresses %)))
+       (filter #(and (not (.isLoopbackAddress %))
+                     (.isSiteLocalAddress %)))  ;; Filters non-local IPs
+       (map str)
+       first)) ;; Gets the first non-local IP
+(def get-local-ip get-non-local-ip)
